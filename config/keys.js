cb37405cf5ec49e027be4a7d4764e79c419b46e5
Brandon Hill <bhill010@gmail.com>
2018-01-28 01:24:43 -0800

    Install mongoose and refactor route file

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://bhill010:monkey06@ds117868.mlab.com:17868/mern-practice-2'
+}
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there'})
-});
+require('./routes/userRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
