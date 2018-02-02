97f97e0db14317ac58d53a98eef5ec24f5cf2491
Brandon Hill <bhill010@gmail.com>
2018-02-01 22:51:14 -0800

    Create new user from register post route

--- routes/credentialRoutes.js
+++ routes/credentialRoutes.js
@@ -2,9 +2,19 @@ const mongoose = require("mongoose");
 var express = require('express');
 var router = express.Router();
 const Credential = require("../models/credential");
+const passport = require("passport");
+
 
 module.exports = (app) => {
   app.post("/register", function(req, res) {
-    res.send("register post route")
+    Credential.register(new Credential({ username: req.body.username}), req.body.password, function(err, credential){
+      if(err) {
+        console.log(err);
+      }
+      passport.authenticate("local")(req, res, function(){
+        res.send("Success register")
+        res.send(credential)
+      })
+    })
   })
 }
